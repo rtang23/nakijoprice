bb50650974da2696766f284b7615013b7e14cf28
Barry <barryxwu@gmail.com>
2020-01-12 12:34:46 +1100

    Able to scrape card URLs from a collection page

--- js-scripts/webscrape.js
+++ js-scripts/webscrape.js
@@ -1,27 +1,27 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require("puppeteer");
 
 async function scrapeProduct(url) {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto(url)
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    const [el] = await page.$x('/html/body/div[2]/div/div/div/div[3]/div/div/div[1]/div/a/div[2]/div/div/div[1]')
-    const text = await el.getProperty('textContent')
-    const srcText = await text.jsonValue()
+  // const [el] = await page.$('/html/body/div[2]/div/div/div/div[3]/div/div/div[1]/div/a/div[2]/div/div/div[1]')
+  // const text = await el.getProperty('textContent')
+  // const srcText = await text.jsonValue()
 
-    console.log({srcText})
+  selector = ".product-link";
+  const cardLinks = await page.evaluate(selector => {
+    let elements = Array.from(document.querySelectorAll(selector));
+    let links = elements.map(element => {
+      return element.href;
+    });
+    return links;
+  }, selector);
+  console.log({ cardLinks });
 
-    // const arr = await page.$x('/html/body/div[2]/div/div/div/div[3]/div/div/div[6]/div/a/div[2]/div/div/div[1]')
-    // console.log({arr})
-    // arr.forEach(async (x) => {
-    //     const text = await x.getProperty('textContent')
-    //     const srcText = await text.jsonValue()
-    //     console.log({srcText})
-    // })
-
-    browser.close()
+  browser.close();
 }
 
-url = 'https://www.nakijo.com.au/collections/dark-neostorm-dane'
-scrapeProduct(url)
-
+// url = "https://www.nakijo.com.au/collections/dark-neostorm-dane"
+url ="https://www.nakijo.com.au/collections/ots-tournament-pack-12-op12"
+scrapeProduct(url);
